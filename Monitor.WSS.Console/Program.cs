7ce8bcf4b1df6ca53cede61122cbda1a223578ed
Phill Barnes <phill@xperan.co.uk>
2012-06-29 13:41:30 +0100

    Base Project

---
+++ Monitor.WSS.Console/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Monitor.WSS.Console
+{
+    class Program
+    {
+        static void Log(string s)
+        {
+            
+            System.Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Yellow;
+            MonitorStorageServer("192.168.0.1");
+        }
+
+        protected static void MonitorStorageServer(string address)
+        {
+            AutoResetEvent waiter = new AutoResetEvent(false);
+            Ping pingSender = new Ping();
+
+            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+            // Wait 12 seconds for a reply.
+            int timeout = 12000;
+
+            // Set options for transmission:
+            // The data can go through 64 gateways or routers
+            // before it is destroyed, and the data packet
+            // cannot be fragmented.
+            PingOptions options = new PingOptions(64, true);
+
+            // Send the ping asynchronously.
+            // Use the waiter as the user token.
+            // When the callback completes, it can wake up this thread.
+            pingSender.SendAsync(address, timeout, buffer, options, waiter);
+
+            // Prevent this example application from ending.
+            // A real application should do something useful
+            // when possible.
+            waiter.WaitOne();
+            Log("Ping example completed.");
+            System.Console.ReadLine();
+        }
+        public static void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            // If the operation was canceled, display a message to the user.
+            if (e.Cancelled)
+            {
+                Log("Ping canceled.");
+
+                // Let the main thread resume. 
+                // UserToken is the AutoResetEvent object that the main thread 
+                // is waiting for.
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            // If an error occurred, display the exception to the user.
+            if (e.Error != null)
+            {
+                Log("Ping failed:");
+                Log(e.Error.ToString());
+
+                // Let the main thread resume. 
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            PingReply reply = e.Reply;
+
+            DisplayReply(reply);
+
+            // Let the main thread resume.
+            ((AutoResetEvent)e.UserState).Set();
+        }
+
+        public static void DisplayReply(PingReply reply)
+        {
+            if (reply == null)
+                return;
+
+            Log(String.Format("ping status: {0}", reply.Status));
+            if (reply.Status == IPStatus.Success)
+            {
+                Log(String.Format("Address: {0}", reply.Address.ToString()));
+                Log(String.Format("RoundTrip time: {0}", reply.RoundtripTime));
+                Log(String.Format("Time to live: {0}", reply.Options.Ttl));
+                Log(String.Format("Don't fragment: {0}", reply.Options.DontFragment));
+                Log(String.Format("Buffer size: {0}", reply.Buffer.Length));
+            }
+            else
+            {
+                //failed
+            }
+        }
+
+    }
+}
---
+++ Monitor.WSS.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Monitor.WSS.Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TFG")]
+[assembly: AssemblyProduct("Monitor.WSS.Console")]
+[assembly: AssemblyCopyright("Copyright © TFG 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ec6aa6b-ce4d-41c4-8286-93c0268815bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
